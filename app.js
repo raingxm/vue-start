89d43ebc9b0cc9b6aeafda74d179ba4d3619b09d
raingxm <raingxm@163.com>
2015-10-12 20:36:51 +0800

    add task work fine

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ new Vue({
     el: "#tasks",
 
     data: {
-        heading: "hello world"
+        tasks: [
+            {  body: "go to home", complete: false}
+        ],
+        newTask: ''
+    },
+
+    methods: {
+        addTask: function(e) {
+            e.preventDefault();
+
+            this.tasks.push({
+                body: this.newTask,
+                complete: false
+            });
+
+            this.newTask = '';
+        }
     }
 });
