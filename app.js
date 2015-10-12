943eb33b76e63fb6798444610c0de7f9222b6770
raingxm <raingxm@163.com>
2015-10-12 21:19:51 +0800

    click complete icon to complete task

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ new Vue({
             this.tasks.$remove(task);
         },
 
+        completeTask: function(task) {
+            task.complete = true;
+        },
+
         editTask: function(task) {
             this.removeTask(task);
 
